c0b40a477365fc398176819db9fa43df01305d6a
Christine Chan <cychan300@gmail.com>
2016-06-15 15:59:06 -0500

    Start of pseudocode

---
+++ vcf_to_phylip.py
@@ -0,0 +1,12 @@
+# This python script coverts vcf files into phylip format
+
+
+# Set up temporary files for data to be pushed into 
+
+# Obtain and open VCF file 
+# Take the VCF file and skip over the metadata and split the header into 9 rows
+
+# Find out how many samples there are within the file 
+# Find the total length of the genome  
+
+
