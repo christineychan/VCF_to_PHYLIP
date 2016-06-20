d4839790e20addb7e8801206406afceabfa76ee4
Christine Chan <cychan300@gmail.com>
2016-06-20 15:18:23 -0500

    Update pseudocode

--- vcf_to_phylip.py
+++ vcf_to_phylip.py
@@ -1,12 +1,33 @@
 # This python script coverts vcf files into phylip format
 
+# Modules 
 
 # Set up temporary files for data to be pushed into 
 
-# Obtain and open VCF file 
-# Take the VCF file and skip over the metadata and split the header into 9 rows
+def VCF_parser(vcf_file_name)
+	# Obtain and open VCF file 
 
-# Find out how many samples there are within the file 
-# Find the total length of the genome  
+	# Take the VCF file and skip over the metadata 
+	# Take the 9th row+ and make an array called "Sample_Names"
+	# Figure out how many samples there are in the file (max) 
+
+	# Find the total length of the genome sequences 
+
+	# Make an array of all of the reference alleles (in row 3)
+	# Make an array of all of the alternative alleles (in row 4)
+
+	# Make an array for the genotypes
+
+	# Push into temporary files
+
+def phylip_format(sequences)
+	# Create a dictionary and push information 
+	# Print sample name 
+	# Print sequence 
+
+# Create an if statement that executes both functions and then closes 
+# the temporary files 
+
+
 
 
