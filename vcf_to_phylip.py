b49d31a088718755ff385a6694a2d1a15152b4da
Christine Chan <cychan300@gmail.com>
2016-09-12 22:33:02 -0500

    Add testing files

--- vcf_to_phylip.py
+++ vcf_to_phylip.py
@@ -5,18 +5,19 @@ import tempfile
 import gzip 
 
 # Main Function 
-def VCF_parser(vcf_file_name)
+def VCF_parser(vcf_file_name):
 	sample_count = 0
 	# Set up temporary files for data to be pushed into 
 	with tempfile.NamedTemporaryFile() as vcf:
 		try:
-			vcf = gzip.open('vcf_file_name.txt.gz', 'r')
-			return VCF_data(vcf)
+			vcf = gzip.open(vcf_file_name, 'r') 
+			return VCF_data(vcf) 
 		except IOError:
 			print "Error: File does not seem to exist"
 			 
 	# Array of all of the reference alleles and alternative alleles
-	alleles = ",".join(line[3:4])
+	refAlleles = ",".join(line[3:])
+	altAlleles = ",".join(line[4:])
 	# Parse the row for genotypes
 	genotypes = line.strip().split()[9:]
 
@@ -29,7 +30,7 @@ def VCF_parser(vcf_file_name)
 
 	vcf.close()
 
-def VCF_data(vcf)
+def VCF_data(vcf):
 	for line in vcf:
 		# Take the VCF file and skip over metadata (lines start with '##')
 		if line.startswith('##'):
@@ -41,15 +42,14 @@ def VCF_data(vcf)
 				sequence[i] = ""
 				sample_count += 1 
 
-def VCF_parse_row(row, samples)
+def VCF_parse_row(matrix, alleles, genotypes):
 	# Make a sparse matrix row for the genotypes for each sample
 		# Create a matrix that adjusts the parameters (sequence length and sample)
-		#
 	# Replace '1' in sparse matrix row with ALT alleles 
 	# Include REF alleles within the sequence 
 	
 	
-def phylip_format(sequences)
+# def phylip_format(sequences):
 	# Create a dictionary and push information 
 	# Print sample name 
 	# Print sequence 
