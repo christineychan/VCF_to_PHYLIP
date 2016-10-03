7f8b92d06f3748801e4a021243c842566eb996bf
Christine Chan <cychan300@gmail.com>
2016-10-03 13:01:37 -0500

    fix return value

--- vcf_to_phylip.py
+++ vcf_to_phylip.py
@@ -1,4 +1,4 @@
-# This python script coverts vcf files into phylip format
+#!/usr/bin/python
 
 from sys import argv
 import zipfile
@@ -6,7 +6,7 @@ import gzip
 import tempfile
 
 # Main Function 
-def VCF_parser(vcf_file_name):
+def vcf_parser(vcf_file_name):
 	samples = []
 	ref = []
 	alt = []
@@ -26,7 +26,7 @@ def VCF_parser(vcf_file_name):
 					line=line.strip('\n')
 					line=line.split('\t')
                     # Parse the row for genotypes
-					genotypes.extend(line[9:])
+					genotypes.append(line[9:])
                     # Array of all of the reference alleles and alternative alleles
 					ref.extend(line[3:4])
 					alt.extend(line[4:5])
@@ -36,30 +36,18 @@ def VCF_parser(vcf_file_name):
 	#for i in genotypes:
 			#i = i.replace("|", "")
 	# Samples there are in the file (max) 
-	return len(samples)
+	return genotypes
 	# Find the total length of the genome sequences 
 	#return len(genotypes)
 
 	vcf.close()
 
-def vcf_data(vcf):
-	for line in vcf:
-		# Take the VCF file and skip over metadata (lines start with '##')
-		if '##' in line:
-			pass 
-		# Identify the sample names and VCF headers if the line starts with '#'
-		elif line.startswith('#'):
-			samples = line.split()[9:]
-			for i in samples:
-				sequence[i] = ""
-				sample_count += 1 
-
-def VCF_parse_row(matrix, alleles, genotypes):
+#def VCF_parse_row(matrix, alleles, genotypes):
 	# Make a sparse matrix row for the genotypes for each sample
 		# Create a matrix that adjusts the parameters (sequence length and sample)
 	# Replace '1' in sparse matrix row with ALT alleles 
 	# Include REF alleles within the sequence 
-	return false
+	#return false
 	
 # def phylip_format(sequences):
 	# Create a dictionary and push information 
@@ -69,6 +57,6 @@ def VCF_parse_row(matrix, alleles, genotypes):
 # Create an if statement that executes both functions and then closes 
 # the temporary files 
 
-
+print vcf_parser(input.txt)
 
 
