4b44de97e4b361b685b94057eb71e63bdf48497f
Christine Chan <cychan300@gmail.com>
2016-10-24 13:22:24 -0500

    fix allele array

--- vcf_to_phylip.py
+++ vcf_to_phylip.py
@@ -9,10 +9,9 @@ def VCF_parser(vcf_file_name):
 	samples = []
 	ref = []
 	alt = []
+	alleles = []
 	genotypes =[]
 	matrix = [[],[],[]]
-	alleles = []
-	counter = -1
 	max = 0
 	# Set up temporary files for data to be pushed into 
 	with tempfile.NamedTemporaryFile() as vcf:
@@ -25,45 +24,60 @@ def VCF_parser(vcf_file_name):
 				elif '#' in line:
 					samples = line.strip().split()[9:]
 				else:
-					line=line.strip('\n')
-					line=line.split('\t')
-                    # Array of all of the reference alleles and alternative alleles
-					ref = ''.join(line[3:4])
-					alt = ''.join(line[4:5])
-					# Calculate maximum allele length 
-					if len(ref) < len(alt):
-						ref = ref + "-" * (len(alt)-1)
-					else:
-						alt = alt + "-" * (len(ref)-1)
-					# Start of matrix 
-					counter +=1
-					counter2 =-1
-					for i in line[9:]:
-						i = i.split('|')
-						for x in i:
-							counter2 +=1
-							if x == '0':
-								pass
-							elif x == '1':
-								matrix[0].append(counter)  # Rows
-								matrix[1].append(counter2) # Columns
-								matrix[2].append(int(x))   # Values	
+					VCF_parse_row(line,alleles,matrix)
+					
 		except IOError:
 			print "Error: File does not seem to exist"
 
-	return matrix
+	return alleles
 	# Find the total length of the genome sequences 
 	# Samples there are in the file (max) 
 
 	vcf.close()
 
-#def VCF_parse_row(matrix, alleles, genotypes):
-	# Make a sparse matrix row for the genotypes for each sample
-		# Create a matrix that adjusts the parameters (sequence length and sample)
-		
-	# Replace '1' in sparse matrix row with ALT alleles 
-	# Include REF alleles within the sequence 
-	#return false
+def VCF_parse_row(line, alleles, matrix):
+	row = -1
+	line=line.strip('\n')
+	line=line.split('\t')
+	# Array of all of the reference alleles and alternative alleles
+	# Converts list into str
+	alleles.append(line[3:5])
+	
+	#max_length=max(map(len, alleles))
+	#max_length=max(alleles, key=len)
+	#max_length=max(len(x) for x in alleles)
+
+	max_length=max(map(lambda x: x, alleles))
+	print max_length
+	# for x in alleles:
+	# 	print len
+	# 	if int(len(x)) < max_length:
+	# 		x = x+('-'* int(max_length))
+	# 		print x
+	#ref = ''.join(line[3:4])
+	#alt = ''.join(line[4:5])
+
+	# Calculate maximum allele length 
+	# if len(ref) < len(alt):
+	# 	ref = ref + "-" * (len(alt)-1)
+	# else:
+	# 	alt = alt + "-" * (len(ref)-1)
+	# alleles.append(ref)
+	# alleles.append(alt)
+	# Start of matrix 
+	row +=1
+	counter =-1
+	for i in line[9:]:
+		i = i.split('|')
+		for x in i:
+			counter +=1
+			if x == '0':
+				pass
+			elif x == '1':
+				matrix[0].append(row)  # Rows
+				matrix[1].append(counter) # Columns
+				matrix[2].append(int(x))   # Values	
+	#return alleles
 
 
 # def phylip_format(sequences):
